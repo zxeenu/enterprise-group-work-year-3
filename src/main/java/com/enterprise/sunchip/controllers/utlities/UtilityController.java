71265b270e0741a16679d7fa072eaefb13d9fd77
zxeenu <zeenu.lastorder@gmail.com>
2021-06-29 23:19:45 +0500

    Error page implemented
    Custom error page implemented

--- src/main/java/com/enterprise/sunchip/controllers/utlities/UtilityController.java
+++ src/main/java/com/enterprise/sunchip/controllers/utlities/UtilityController.java
@@ -2,11 +2,13 @@ package main.java.com.enterprise.sunchip.controllers.utlities;
 
 import main.java.com.enterprise.sunchip.services.LocalSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
@@ -20,5 +22,24 @@ public class UtilityController {
         localSession.clearTokenStoredInLocalCashe(request);
         return new ModelAndView("redirect:/Login");
     }
+
+//    @RequestMapping("/error")
+//    public ModelAndView handleError(HttpServletRequest request) {
+//        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+//        ModelAndView mv = new ModelAndView();
+//        mv.setViewName("pages/error/Error");
+//
+//        if (status != null) {
+//            Integer statusCode = Integer.valueOf(status.toString());
+//
+//            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+//                mv.addObject("errorMessage", statusCode);
+//            }
+//            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+//                mv.addObject("errorMessage", statusCode);
+//            }
+//        }
+//        return mv;
+//    }
 
 }
